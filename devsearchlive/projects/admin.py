f48533a25b9330ca73efbc8077c3e1cd198b9bbc
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-06-15 11:29:52 +0300

    Altered admin panel

--- devsearchlive/projects/admin.py
+++ devsearchlive/projects/admin.py
@@ -3,6 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Project, Review, Tag
 
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "created",
+    ]
+    list_filter = ["created"]
+    search_fields = ["title"]
+    fieldsets = [
+        ("About the project", {"fields": ["title", "description"]}),
+        ("Links", {"fields": ["demo_link", "source_link"]}),
+        ("Votes", {"fields": ["vote_total", "vote_ratio"]}),
+        ("Tags", {"fields": ["tags"]}),
+    ]
+
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Review)
 admin.site.register(Tag)
